fc0bca895b5f2f484d18604ecb7b754c32efcaad
vadimbublikov2 <prostoparol123>
2017-10-23 00:27:06 +0300

    add ClassMatcher with test

---
+++ ClassFinder/src/test/java/ClassMatcherTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassMatcherTest {
+
+    @Test
+    public void patternParts() {
+        ClassMatcher classMatcher = new ClassMatcher("Fi*InputS ")
+    }
+
+}
